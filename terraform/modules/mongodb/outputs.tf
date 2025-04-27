fd13a1f267c583632f96c951284396a5982770cd
Roshan <roshansanjur@gmail.com>
2025-04-27 13:14:25 +0530

    error handling

--- terraform/modules/mongodb/outputs.tf
+++ terraform/modules/mongodb/outputs.tf
@@ -5,7 +5,7 @@ output "endpoint" {
 
 output "connection_string" {
   description = "Connection string for DocumentDB"
-  value       = "mongodb://${var.db_username}:${var.db_password}@${aws_docdb_cluster.main.endpoint}:27017/spearpoint?retryWrites=false"
+  value = "mongodb://${var.db_username}:${var.db_password}@${aws_docdb_cluster.main.endpoint}:27017/spearpoint?retryWrites=false&authSource=admin"
   sensitive   = true
 }
 
