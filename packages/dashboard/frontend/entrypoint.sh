ac3e52f94818001b073a1fa91a47f5356de01f96
Roshan <roshansanjur@gmail.com>
2025-04-27 13:31:47 +0530

    fixes

--- packages/dashboard/frontend/entrypoint.sh
+++ packages/dashboard/frontend/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+
+# Debug: Print the BACKEND_URL value
+echo "Using BACKEND_URL: ${BACKEND_URL}"
+
 # Replace environment variables in the nginx config
 envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Debug: Print the generated nginx config
+echo "Generated Nginx configuration:"
+cat /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
